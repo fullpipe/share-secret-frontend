966a1e8a93390a0dbc47af6cbac0ecdfa3907b94
Eugene Bravov <eugene.bravov@gmail.com>
2020-12-12 14:50:41 +0300

    Add unseal component

---
+++ src/app/unseal/unseal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unseal',
+  templateUrl: './unseal.component.html',
+  styleUrls: ['./unseal.component.scss']
+})
+export class UnsealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
