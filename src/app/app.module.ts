fb2aa416ef6a3dbd83e0dce9c2ed59d21ca0b392
Eugene Bravov <eugene.bravov@gmail.com>
2020-12-24 23:41:12 +0300

    Add error translations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,32 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { RouterModule } from '@angular/router';
 import { UnsealComponent } from './unseal/unseal.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
     declarations: [AppComponent, SealComponent, WelcomeComponent, HowItWorksComponent, UnsealComponent],
-    imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule, RouterModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        RouterModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient],
+            },
+        }),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
